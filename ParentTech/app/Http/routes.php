142f2a691b007571baef30de640724d06e778506
bandala <bandala@bandalas-MacBook-Pro.local>
2018-05-19 23:05:33 -0500

    implemented database, inserted data and created relationships

--- ParentTech/app/Http/routes.php
+++ ParentTech/app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Profile;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -16,3 +18,45 @@ Route::get('/', function () {
 });
 
 Route::resource('/signup','UserController@displaySignUpView');
+
+/*
+|--------------------------------------------------------------------------
+| Database Application Testing Routes
+|--------------------------------------------------------------------------
+|
+| All routes related with database testing,
+| will be deleted, it's just for testing purposes
+|
+|
+*/
+
+Route::get('/insert-user',function(){
+
+  DB::insert('insert into users(username,email,password) values(?, ?, ?)',['kbandala','k.rbandala@gmail.com','123test']);
+
+});
+
+Route::get('/insert-profile',function(){
+  DB::insert('insert into profiles(user_id,name,lastname,sex,birth) values(?,?,?,?,?)',[1,'karla','robledo',1,'2018-01-01']);
+});
+
+Route::get('/get-user/{id}',function($id){
+
+  $results = DB::select('select * from users where id = ?',[$id]);
+  return $results;
+
+});
+
+//  One to One relationship
+
+//  Get the profile that belongs to that user
+Route::get('/user/{id}/profile',function($id){
+
+  return User::find($id)->profile;
+
+});
+
+//  Get the user that belongs to that profile
+Route::get('/profile/{id}/user',function($id){
+  return Profile::find($id)->user;
+});
--- ParentTech/app/Profile.php
+++ ParentTech/app/Profile.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+
+  protected $fillable = [
+      'name', 'lastname', 'sex','birth'
+  ];
+
+  public function user(){
+    return $this->belongsTo('App\User');
+  }
 }
