bf5311ac9dfaff0882163586d7ddf5c42fa452c6
bandala <bandala@bandalas-MacBook-Pro.local>
2018-06-26 23:48:35 -0500

    LoginController

--- parenttech/app/Http/Controllers/Auth/AdminLoginController.php
+++ parenttech/app/Http/Controllers/Auth/AdminLoginController.php
@@ -28,6 +28,7 @@ class AdminLoginController extends Controller
             'password' => 'required|min:6'
         ]);
 
+
         if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember))
         {
             return redirect()->intended(route('admin.dashboard'));
